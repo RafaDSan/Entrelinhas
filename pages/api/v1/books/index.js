90a1425d1ebc0cd7ea0125d9129abdd2f851c77c
RafaDSan <rafaeldss12@gmail.com>
2025-09-02 15:13:40 -0300

    add `controller.errorHandlers` in books

--- pages/api/v1/books/index.js
+++ pages/api/v1/books/index.js
@@ -1,12 +1,15 @@
 import { createRouter } from "next-connect";
-import book from "models/book.js"
+import controller from "infra/controller";
+import book from "models/book.js";
 
 const router = createRouter();
 
 router.post(postHandler);
 
+export default router.handler(controller.errorHandlers);
+
 async function postHandler(request, response) {
-    const bookInputValues = request.body;
-    const newBook = await book.create(bookInputValues);
-    return response.status(201).json(newBook);
+  const bookInputValues = request.body;
+  const newBook = await book.create(bookInputValues);
+  return response.status(201).json(newBook);
 }
